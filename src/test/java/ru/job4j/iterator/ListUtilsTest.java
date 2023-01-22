368c1e86a3822847c65dd373e03d9f62a760ca93
Kotov Oleg <grum1972@gmail.com>
2023-01-22 21:10:14 +0300

    7. ListIterator [#350217 #262374]

---
+++ src/test/java/ru/job4j/iterator/ListUtilsTest.java
@@ -0,0 +1,64 @@
+package ru.job4j.iterator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+class ListUtilsTest {
+
+    private List<Integer> input;
+
+    @BeforeEach
+    void setUp() {
+        input = new ArrayList<>(Arrays.asList(1, 3));
+    }
+
+    @Test
+    void whenAddBefore() {
+        ListUtils.addBefore(input, 1, 2);
+        assertThat(input).hasSize(3).containsSequence(1, 2, 3);
+    }
+
+    @Test
+    void whenAddBeforeWithInvalidIndex() {
+        assertThatThrownBy(() -> ListUtils.addBefore(input, 0, 2))
+                .isInstanceOf(IndexOutOfBoundsException.class);
+    }
+
+    @Test
+    void whenAddAfter() {
+        ListUtils.addAfter(input, 0, 2);
+        assertThat(input).hasSize(3).containsSequence(1, 2, 3);
+    }
+
+    @Test
+    void whenAfterBeforeWithInvalidIndex() {
+        assertThatThrownBy(() -> ListUtils.addBefore(input, 3, 2))
+                .isInstanceOf(IndexOutOfBoundsException.class);
+    }
+
+    @Test
+    void whenRemoveIf() {
+        input = new ArrayList<>(Arrays.asList(1, 2, 3));
+        ListUtils.removeIf(input, x -> x > 2);
+        assertThat(input).hasSize(2).containsSequence(1, 2);
+    }
+
+    @Test
+    void whenReplaceIf() {
+        input = new ArrayList<>(Arrays.asList(1, 2, 3));
+        ListUtils.replaceIf(input, x -> x > 1, 0);
+        assertThat(input).hasSize(3).containsSequence(1, 0, 0);
+    }
+
+    @Test
+    void whenRemoveAll() {
+        input = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+        ListUtils.removeAll(input,  new ArrayList<>(Arrays.asList(2, 3)));
+        assertThat(input).hasSize(2).containsSequence(1, 4);
+    }
+}
