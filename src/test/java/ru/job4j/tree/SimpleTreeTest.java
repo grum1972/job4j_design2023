77337649bc7e735b29413efabdd46300081b4af5
Kotov Oleg <grum1972@gmail.com>
2023-01-26 21:20:49 +0300

    1. Создать элементарную структуру дерева [#1711 #267456]

--- src/test/java/ru/job4j/tree/SimpleTreeTest.java
+++ src/test/java/ru/job4j/tree/SimpleTreeTest.java
@@ -47,4 +47,15 @@ class SimpleTreeTest {
         assertThat(tree.add(2, 4)).isFalse();
     }
 
+    @Test
+    void isBinaryTree() {
+        Tree<Integer> tree = new SimpleTree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(1, 4);
+        tree.add(4, 5);
+        tree.add(5, 6);
+        assertThat(tree.isBinary()).isFalse();
+    }
+
 }
