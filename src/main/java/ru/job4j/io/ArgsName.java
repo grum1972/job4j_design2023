ac79fdbfc85ed3f67ad3143d3b1fc7e80730a704
Kotov Oleg <grum1972@gmail.com>
2023-02-01 21:36:01 +0300

    13. Именованные аргументы [#295518 #265672] перелал валидацию ключа

--- src/main/java/ru/job4j/io/ArgsName.java
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -17,14 +17,14 @@ public class ArgsName {
         if (words.length != 2) {
             throw new IllegalArgumentException("You need input pair key=value");
         }
+        if (words[0].length() == 0) {
+            throw new IllegalArgumentException("Is not key ");
+        }
         if (!words[0].contains("-")) {
             throw new IllegalArgumentException("Key must have start with symbol -");
         }
         if (words[0].contains("-") && words[0].length() == 1) {
             throw new IllegalArgumentException("Is not key after symbol -");
-        }
-        if (words[0].length() == 0) {
-            throw new IllegalArgumentException("Is not key ");
         }
         if (words[1].length() == 0) {
             throw new IllegalArgumentException("Value is absent");
@@ -49,7 +49,7 @@ public class ArgsName {
     }
 
     public static void main(String[] args) {
-        ArgsName jvm = ArgsName.of(new String[]{"-Xmx=512", "-encoding=UTF-8"});
+        ArgsName jvm = ArgsName.of(new String[]{"=512"});
         System.out.println(jvm.get("Xmx"));
 
         ArgsName zip = ArgsName.of(new String[]{"-out=project.zip", "-encoding=UTF-8"});
